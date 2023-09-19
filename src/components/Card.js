a72203880d7cbb5600fc087449feb28ed1d33e0f
Piper Maru <fourdd4@gmail.com>
2023-09-19 11:13:58 +0300

    fix: submit name & style import

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,28 @@
 import React from "react";
 
 export default function Card(props) {
-  function handleClick() {
-    props.onCardClick(props.card);
-  }
+    function handleClick() {
+        props.onCardClick(props.card);
+    }
 
-  return (
-    <article className="card" key={props.card._id}>
-      <img
-        className="card__image"
-        src={props.card.link}
-        alt={props.card.name}
-        onClick={handleClick}
-      />
-      <button type="button" className="card__delete-button" />
-      <div className="card__description">
-        <h2 className="card__title" />
-        <div className="card__like">
-          <button type="button" className="card__like-button" />
-          <p className="card__like-counter">{props.card.likes.length}</p>
-        </div>
-      </div>
-    </article>
-  );
+    return (
+        <article className="card">
+            <img
+                className="card__image"
+                src={props.card.link}
+                alt={props.card.name}
+                onClick={handleClick}
+            />
+            <button type="button" className="card__delete-button" />
+            <div className="card__description">
+                <h2 className="card__title" />
+                <div className="card__like">
+                    <button type="button" className="card__like-button" />
+                    <p className="card__like-counter">
+                        {props.card.likes.length}
+                    </p>
+                </div>
+            </div>
+        </article>
+    );
 }
